575d1270d6fcdcf8fd45a34ee75ec0e361f5b1a0
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-09 23:01:52 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   enable_dns_hostnames = true
   enable_dns_support = true
-  tags = {
-    Name = "Project VPC"
-  }
+  tags = merge(var.tags, { Name = "${var.env}-vpc" })
+
 }
