1b65b1c4b0c1948eeb92374a0e499a6edf8e29dd
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-26 19:34:53 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ resource "aws_vpc" "main" {
   tags = merge(var.tags, { Name = "${var.env}-vpc" })
 
 }
+
+module "subnets" {
+  source = "./subnets"
+
+  for_each = var.subnets
+  vpc_id = aws_vpc.main.id
+  cidr_block = each.value["cidr_block"]
+
+  tags = var.tags
+  env = var.env
+  name = each.value["name"]
+}
