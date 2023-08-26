bf315f2977c726310aaeed99816d927284c8bd4b
ThumburuAditya <chandramouliadityat@gmail.com>
2023-08-26 19:54:37 +0530

    Adding the code

--- main.tf
+++ main.tf
@@ -18,3 +18,8 @@ module "subnets" {
   name = each.value["name"]
   azs = each.value["azs"]
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tags, { Name = "${var.env}-igw" })
+}
